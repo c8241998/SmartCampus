b24cd7c661033403209309a57de0abb1c46a871d
c8241998 <569362884@qq.com>
2019-05-04 21:50:15 +0800

    Initial commit

---
+++ apps/account/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, created_at, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            created_at=created_at,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email,created_at, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            created_at=created_at,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=40,
+        unique=True,
+    )
+    username = models.CharField(
+        max_length=40,
+        primary_key=True
+    )
+    created_at = models.DateField()
+
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['created_at']
+
+    def __str__(self):
+        return self.email
---
+++ apps/account/views.py
@@ -0,0 +1,55 @@
+from datetime import time
+from django.contrib import auth
+from django.core.serializers import json
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from apps.account import models
+
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        try:
+            re = models.MyUser.objects.get(email=email)
+        except models.MyUser.DoesNotExist:
+            res = {'msg': 'fail', 'info': 'email not found.'}
+            return HttpResponse(json.dumps(res))
+        re = auth.authenticate(request, username=email, password=password)
+        if re is None:
+            res = {'msg': 'fail', 'info': 'password is invalid.'}
+            return HttpResponse(json.dumps(res))
+        auth.login(request, re)
+
+        res = {'msg': 'success'}
+        return HttpResponse(json.dumps(res))
+
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        username = request.POST.get('username')
+        try:
+            re = models.MyUser.objects.get(email=email)
+            res = {'msg': 'fail', 'info': 'email already taken.'}
+            return HttpResponse(json.dumps(res))
+        except models.MyUser.DoesNotExist:
+            try:
+                re = models.MyUser.objects.get(username=username)
+                res = {'msg': 'fail', 'info': 'username already taken.'}
+                return HttpResponse(json.dumps(res))
+            except models.MyUser.DoesNotExist:
+                user = models.MyUser()
+                user.email = email
+                user.set_password(password)
+                user.username = username
+                user.created_at = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+                user.save()
+                res = {'msg': 'success'}
+                return HttpResponse(json.dumps(res))
+
+
+@login_required
+def logout(request):
+    auth.logout(request)
